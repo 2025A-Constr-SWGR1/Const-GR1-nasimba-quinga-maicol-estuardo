3cb11e8844cb654b7a7b88aa3d2f0b6320403f3a
Maicol Nasimba <maicol.nasimba@epn.edu.ec>
2025-06-20 15:46:13 -0500

    Clase-20-06-2025

--- calculadora/src/app.controller.ts
+++ calculadora/src/app.controller.ts
@@ -1,9 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private readonly configService: ConfigService,
+  ) {}
+
+  @Get('database')
+  getDatabaseInformation(){
+    return {
+      DB_HOST: this.configService.get<string>('DB_HOST'),
+      DB_USER: this.configService.get<string>('DB_USER'),
+      DB_PASSWORD: this.configService.get<string>('DB_PASSWORD'),
+      DB_NAME: process.env.DB_NAME, 
+    }
+  }
 
   @Get()
   getHello(): string {
