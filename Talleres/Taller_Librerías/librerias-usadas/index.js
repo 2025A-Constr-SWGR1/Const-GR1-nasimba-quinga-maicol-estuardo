a0d9fea61e6315410ca734cdc7b65cd828d624d6
Maicol Nasimba <maicol.nasimba@epn.edu.ec>
2025-08-02 00:27:28 -0500

    Taller_Librerías

---
+++ Talleres/Taller_Librerías/librerias-usadas/index.js
@@ -0,0 +1,5 @@
+const paqueteMultiplicacion = require('2025a-swgr1-menq-multiplicacion');
+
+const respuesta = paqueteMultiplicacion.multiplicacion(5, 3);
+
+console.log('La respuesta es: ', respuesta);
